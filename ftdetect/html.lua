8795d7651b4c4b41a8d571c4034eeac28ef8faca
Trent Deal <trent.deal@gmail.com>
2022-08-08 19:31:56 -0400

    Rewrite ftdetect/html.vim in lua

---
+++ ftdetect/html.lua
@@ -0,0 +1,20 @@
+local fn = vim.fn
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+
+local DetectGoTemplate = augroup("hugo-nvim-DetectGoTemplate", { clear = true })
+
+autocmd({ "BufNewFile", "BufRead" }, {
+    group = DetectGoTemplate,
+    pattern = "*.html",
+    callback = function()
+        local curdir = fn.getcwd()
+
+        -- verify by checking if the current directory has the archetypes folder
+        local is_hugo = fn.empty(fn.glob(curdir .. "/archetypes")) == 0
+
+        if is_hugo then
+            vim.opt_local.filetype = "htmlhugo"
+        end
+    end
+})
